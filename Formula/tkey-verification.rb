552431d97d1cbe3ed58530dcb83fa38ca0b20183
dehanj <jobson@tillitis.se>
2024-06-03 10:58:45 +0200

    tkey-verification: update to version 0.0.3

--- Formula/tkey-verification.rb
+++ Formula/tkey-verification.rb
@@ -1,12 +1,12 @@
 class TkeyVerification < Formula
   desc "Tkey Device Verification: verifies the authenticity of your TKey"
   homepage "https://github.com/tillits/tkey-verification"
-  url "https://github.com/tillitis/tkey-verification/releases/download/v0.0.2/tkey-verification_0.0.2_macos-universal.tar.gz"
-  sha256 "c8585f439e9ef0a9d86ba2a290f210cc4f78646e05e5a7be34169551dc6bf579"
-  version "0.0.2"
+  url "https://github.com/tillitis/tkey-verification/releases/download/v0.0.3/tkey-verification_0.0.3_macos-universal.tar.gz"
+  sha256 "2fb0a55d6432f6e1876a0a19db16d8ad7ae0328e19f401bde04fb3a285cb5605"
+  version "0.0.3"
   license "GPL-2.0"
 
   def install
-    bin.install "tkey-verification_0.0.2_macos-universal" => "tkey-verification"
+    bin.install "tkey-verification_0.0.3_macos-universal" => "tkey-verification"
   end
 end
