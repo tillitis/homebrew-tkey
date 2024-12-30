b99f75fd2a537c9d253d647a89b52d39bccf700e
Daniel Jobson <jobson@tillitis.se>
2024-12-30 11:44:39 +0100

    tkey-sign: update to v1.0.1

--- Formula/tkey-sign.rb
+++ Formula/tkey-sign.rb
@@ -5,12 +5,12 @@
 class TkeySign < Formula
   desc "Tool to digitally sign files with TKey"
   homepage "https://tillitis.se/"
-  version "1.0.0"
+  version "1.0.1"
   license "GPLv2"
 
   on_macos do
-    url "https://github.com/tillitis/tkey-sign-cli/releases/download/v1.0.0/tkey-sign_1.0.0_Darwin_universal.tar.gz"
-    sha256 "5a0045010543596971c5f2a4cfa666d61e7b6adc3e00a7433c7b8f4f3921a7d8"
+    url "https://github.com/tillitis/tkey-sign-cli/releases/download/v1.0.1/tkey-sign_1.0.1_Darwin_universal.tar.gz"
+    sha256 "beadbb1fd9080a433091da8948663b78587ad8cfe429f76138d1b99950039992"
 
     def install
       bin.install "tkey-sign"
@@ -19,22 +19,26 @@ class TkeySign < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/tillitis/tkey-sign-cli/releases/download/v1.0.0/tkey-sign_1.0.0_Linux_arm64.tar.gz"
-      sha256 "a15197ef6af5051fed2b723636b6c2ec9bf6cf6d903c78fff7965ed3a023f2ab"
+    if Hardware::CPU.intel?
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/tillitis/tkey-sign-cli/releases/download/v1.0.1/tkey-sign_1.0.1_Linux_amd64.tar.gz"
+        sha256 "81a796ce0f1f4bd55ff9e5aa81ca1c451f98005934079af39d94d5c679eb4857"
 
-      def install
-        bin.install "tkey-sign"
-        man1.install "man/tkey-sign.1"
+        def install
+          bin.install "tkey-sign"
+          man1.install "man/tkey-sign.1"
+        end
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/tillitis/tkey-sign-cli/releases/download/v1.0.0/tkey-sign_1.0.0_Linux_amd64.tar.gz"
-      sha256 "bbc45c28f7885d10baa7136b37885650122362b2125ab65608ca05fe0f8d26fd"
+    if Hardware::CPU.arm?
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/tillitis/tkey-sign-cli/releases/download/v1.0.1/tkey-sign_1.0.1_Linux_arm64.tar.gz"
+        sha256 "270ea0b137774e1a9833760ed995d34d7da0feaaab22b792a48a7ee1191b15b0"
 
-      def install
-        bin.install "tkey-sign"
-        man1.install "man/tkey-sign.1"
+        def install
+          bin.install "tkey-sign"
+          man1.install "man/tkey-sign.1"
+        end
       end
     end
   end
