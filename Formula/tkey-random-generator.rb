b224923d7f833a472ff09062a8f0dbc7865b9003
Daniel Jobson <jobson@tillitis.se>
2026-03-17 08:23:55 +0100

    tkey-random-generator: bump to version 0.0.3

--- Formula/tkey-random-generator.rb
+++ Formula/tkey-random-generator.rb
@@ -1,13 +1,13 @@
 class TkeyRandomGenerator < Formula
   desc "Tkey Random Generator: hardware backed high-quality random numbers"
   homepage "https://github.com/tillits/tkey-random-generator"
-  url "https://github.com/tillitis/tkey-random-generator/releases/download/v0.0.2/tkey-random-generator_0.0.2_macos-universal.tar.gz"
-  sha256 "8e8a1fd5de2253106c91595a9f38095f588f23ac328e4e87f82a3fbe31934f09"
-  version "0.0.2"
+  url "https://github.com/tillitis/tkey-random-generator/releases/download/v0.0.3/tkey-random-generator_0.0.3_darwin-universal.tar.gz"
+  sha256 "9af97581cfed30edb1072c1752d87f9979bca30de007192d5455576cc6de3fca"
+  version "0.0.3"
   license "GPL-2.0"
 
   def install
-    bin.install "tkey-random-generator_0.0.2_macos-universal" => "tkey-random-generator"
+    bin.install "tkey-random-generator_0.0.3_darwin-universal" => "tkey-random-generator"
     man1.install "tkey-random-generator.1"
   end
 end
